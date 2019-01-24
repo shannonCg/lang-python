27171531710fb0489987360fe9ae2acb18d488b0
shannon <shannon_chang@syntrontech.com>
2019-01-24 16:43:48 +0800

    add docString practice

--- python_learn/function/function.py
+++ python_learn/function/function.py
@@ -30,3 +30,19 @@ def cylinder_volume(height, radius):
 print("the cylinder volume with height {} and radius {} is {}".format(10, 3, cylinder_volume(10, 3)))
 print("the cylinder volume with height {} and radius {} is {}".format(10, 5, cylinder_volume(radius=5, height=10)))
 print()
+
+#docStrings: you can write the description for function
+def readable_timedelta(days):
+    #use the Google format
+    """Return a string of the number of weeks and days included in days.
+
+    Args:
+        days (int): number of days to convert
+    Returns:
+        string of the number of weeks and days included in days
+    """
+    weeks = days // 7
+    remainder = days % 7
+    return  "{} week(s) and {} day(s)".format(weeks, remainder)
+
+print(readable_timedelta(10))
